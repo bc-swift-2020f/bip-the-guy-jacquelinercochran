91b81ff7c063de57b2fb7e22eccbc6ca65d4b49a
Jackie Cochran <cochrajg@bc.edu>
2020-09-19 14:31:01 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Jackie Cochran on 9/19/20.
+//  Copyright © 2020 Jackie Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
